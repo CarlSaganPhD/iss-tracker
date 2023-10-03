12ec20f4cc1fa7f22e73ff998be57853781376af
Carlsaganphd <ryan@marcomstrategies.co>
2023-10-03 08:34:07 -0700

    velocity altitude update

--- main.py
+++ main.py
@@ -18,8 +18,17 @@ def index():
 @app.route('/iss_location')
 def iss_location():
     response = requests.get('https://api.wheretheiss.at/v1/satellites/25544')
-    app.logger.info(f"Fetching ISS location: Latitude {response.json().get('latitude')} Longitude {response.json().get('longitude')}")
-    return jsonify(response.json())
+    iss_data = response.json()
+    app.logger.info(f"Fetching ISS location: Latitude {iss_data.get('latitude')} Longitude {iss_data.get('longitude')}")
+    return jsonify({
+        'latitude': iss_data.get('latitude'),
+        'longitude': iss_data.get('longitude'),
+        'altitude': iss_data.get('altitude'),  # Altitude of the ISS in kilometers
+        'velocity': iss_data.get('velocity'),  # Velocity of the ISS in kilometers per hour
+        'visibility': iss_data.get('visibility'),  # Whether the ISS is in daylight or nighttime
+        'footprint': iss_data.get('footprint'),  # Diameter of the ground area which the ISS can be seen from
+        'timestamp': iss_data.get('timestamp')  # Timestamp of the data
+    })
 
 # Use the PORT environment variable or default to 5000
 port = int(os.environ.get("PORT", 5000))
